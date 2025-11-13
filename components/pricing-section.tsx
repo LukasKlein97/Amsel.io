87b76cacbab1a15c399a1f063b118e271a252ca6
LukasKlein97 <luklein13@gmail.com>
2025-11-13 12:35:25 +0200

    pricing

--- components/pricing-section.tsx
+++ components/pricing-section.tsx
@@ -1,13 +1,7 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Check } from "lucide-react";
 
 export function PricingSection() {
